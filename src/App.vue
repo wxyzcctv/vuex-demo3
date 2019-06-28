9eef7ff1306b1a446c8963575e979a5f2262593e
wxyzcctv <2846629226@qq.com>
2019-06-28 21:41:12 +0800

    使用pm i -S core-js@2.5.7解决es6语法问题

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>购物车示例</h1>
+    <p>账号: {{email}}</p>
+    <hr>
+    <h2>产品</h2>
+    <ProductList/>
+    <hr>
+    <h2>清单</h2>
+    <ShoppingCart/>
+  </div>
+</template>
+ 
+<script>
+import { mapState } from 'vuex'
+import ProductList from  './components/ProductList'
+import ShoppingCart from  './components/ShoppingCart'
+
+export default {
+  name: 'app',
+  computed: mapState({
+    email: state => state.userInfo.email
+  }),
+  components: { ProductList, ShoppingCart  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/store/modules/products.js
@@ -0,0 +1,49 @@
+import shop from '../../api/shop'
+import {PRODUCTS} from '../mutation-types'
+
+// initial state
+const state = {
+  all: []
+}
+
+// getters
+const getters = {}
+
+// actions
+const actions = { 
+  getAllProducts ({ commit }) {
+    shop.getProducts(products => {
+      commit(PRODUCTS.SET_PRODUCTS, products)
+    })
+    // 上面的进行翻译之后就是：
+    // shop.getProducts(
+    //   function(products){
+    //     return commit(PRODUCTS.SET_PRODUCTS,products)
+    //   }
+    // )
+    // shop.getProducts是一个延时函数，并在这个延时函数在shop中得到所有产品的数据信息。
+    // 这里内部传入的products就是shop.getProducts中传入的_products，也就是产品详细消息。
+    // PRODUCTS.SET_PRODUCTS也是一个函数，在mutations中定义的，主要作用就是获取全部的产品
+  }
+}
+
+// mutations
+const mutations = {
+  [PRODUCTS.SET_PRODUCTS] (state, products) {
+    state.all = products
+  },
+
+  [PRODUCTS.DECREMENT_PRODUCT_INVENTORY] (state, { id }) {
+    const product = state.all.find(product => product.id === id)
+    // find()的用法是arr.find(function(){})找出指定数组中通过find函数中筛选的内容
+    product.inventory--
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
