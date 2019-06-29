eb1b9e9d374c5a3d2c44e85b6f590b04be8a02c2
wxyzcctv <2846629226@qq.com>
2019-06-29 15:20:27 +0800

    添加产品展示区

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,1 +1,82 @@
- 
+import shop from '../../api/shop'
+import { CART, PRODUCTS } from '../mutation-types'
+
+const state = {
+    items: [],
+    checkoutStatus: null
+}
+
+const getter = {
+    cartProducts:(state,rootState) =>{
+        return state.items.map(({id,quantity}) =>{
+            const product = rootState.products.all.find( product => product.id === id)
+            return {
+                title:product.title,
+                price:product.price,
+                quantity
+            }
+        })
+    },
+    carTotalPrice:(state,getter) => {
+        return getter.cartProducts.reduce((product,total)=>{
+            return total + product.price * product.quantity
+        },0)
+    }
+}
+
+const actions = {
+    checkout({state,commit},products){
+        const saveCartItems = [...state.items]
+        commit(CART.SET_CART_ITEMS,{items:[]})
+        commit(CART.SET_CHECKOUT_STATUS,null)
+        shop.buyProducts(
+            products,
+            ()=>commit(CART.SET_CHECKOUT_STATUS,'sucesseful'),
+            ()=>{
+                commit(CART.SET_CHECKOUT_STATUS,'failed'),
+                commit(CART.SET_CART_ITEMS,{items:saveCartItems})
+            }
+        )
+    },
+
+    addProductToCart({state,commit},product){
+        commit(CART.SET_CHECKOUT_STATUS,null)
+        if(product.inventory > 0){
+            const cartItem = state.items.find(item => item.id === product.id)
+            if(!cartItem){
+                commit(CART.PUSH_PRODUCT_TO_CART,{id:product.id})
+            }else{
+                commit(CART.INCREMENT_ITEM_QUANTITY,{id:item.id})
+                // 注意这里有所不同
+            }
+            commit(`product/${PRODUCTS.DECREMENT_PRODUCT_INVENTORY}`,{id:product.id},{root:true})
+        }
+    }
+
+}
+
+const mutations = {
+    [CART.PUSH_PRODUCT_TO_CART](state,{id}){
+        state.items.push({
+            id,
+            quantity:1
+        })
+    },
+    [CART.INCREMENT_ITEM_QUANTITY](state,{id}){
+        const cartItem = state.items.find(item => item.id === id)
+        cartItem.quantity++
+    },
+    [CART.SET_CART_ITEMS](state,{ items }){
+        state.items = items
+    },
+    [CART.SET_CHECKOUT_STATUS](state,status){
+        state.checkoutStatus = status
+    }
+}
+export default {
+    namespaced: true,
+    state,
+    getter,
+    actions,
+    mutations,
+}
